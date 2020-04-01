dcd5201faef58179ff63ed01375a25a449561b62
Gaurav <gauravmi@thoughtworks.com>
2020-04-01 20:07:54 +0530

    debug: docker compose deploy not working

--- aws_check_cluster_status.sh
+++ aws_check_cluster_status.sh
@@ -5,9 +5,9 @@ aws configure set aws_secret_access_key $AWS_SECRET_KEY
 
 while true; do
     eval status=`aws ecs describe-clusters --cluster ec2-catalyst-cluster-ci --region ap-south-1 | jq .clusters[].status`
+    aws ecs describe-clusters --cluster ec2-catalyst-cluster-ci --region ap-south-1
     if [ "$status" == "ACTIVE" ]; then
         break
     fi
-    aws ecs describe-clusters --cluster ec2-catalyst-cluster-ci --region ap-south-1
     sleep 10
 done
