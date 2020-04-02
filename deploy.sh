da9fcfa8b8d0752c940614d3f6f7fb3fac4bf042
Gaurav <gauravmi@thoughtworks.com>
2020-04-02 18:09:33 +0530

    using environment as identiifier for service names in compose; bringing down containers befoore deploying new compose file

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export BOOKING_IMAGE=$1
+export ENVIRONMENT=$2
 
 apt-get install jq -y
 curl -o /usr/local/bin/ecs-cli https://amazon-ecs-cli.s3.amazonaws.com/ecs-cli-linux-amd64-latest
@@ -15,13 +16,13 @@ ecs-cli configure profile --access-key $AWS_ACCESS_KEY --secret-key $AWS_SECRET_
 ecs-cli registry-creds up ./creds_input_file.yml --role-name catalystSecretsExecutionRoleCI
 
 echo  "using image.. $BOOKING_IMAGE"
-ecs-cli compose --cluster-config $CLUSTER_CONFIG_NAME --ecs-profile $CLUSTER_PROFILE_NAME up --create-log-groups
+ecs-cli compose down --cluster-config $CLUSTER_CONFIG_NAME --ecs-profile $CLUSTER_PROFILE_NAME
+ecs-cli compose --cluster-config $CLUSTER_CONFIG_NAME --ecs-profile $CLUSTER_PROFILE_NAME --project-name $ENVIRONMENT up --create-log-groups
 
 ecs-cli compose down --cluster-config $CLUSTER_CONFIG_NAME --ecs-profile $CLUSTER_PROFILE_NAME
 ecs-cli compose service up --cluster-config $CLUSTER_CONFIG_NAME --ecs-profile $CLUSTER_PROFILE_NAME
 
 ecs-cli ps --cluster-config $CLUSTER_CONFIG_NAME --ecs-profile $CLUSTER_PROFILE_NAME
-
 ecs-cli compose service ps --cluster-config $CLUSTER_CONFIG_NAME --ecs-profile $CLUSTER_PROFILE_NAME
 
 rm -rf ecs-registry-creds_*.yml
