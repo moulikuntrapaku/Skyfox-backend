23a5f4a679a5f86e9a81534b1437806babac7468
Gaurav <gauravmi@thoughtworks.com>
2020-04-02 22:36:45 +0530

    debug: removing service creation

--- deploy.sh
+++ deploy.sh
@@ -3,30 +3,28 @@
 export TAG=$3
 
 # used  in compose file
-export BOOKING_IMAGE=$1:$TAG
-export POSTGRES_IMAGE=$2:$TAG
+export COMMIT_SHA=$3
+export ENVIRONMENT=$4
+
+export BOOKING_IMAGE=$1:$COMMIT_SHA-$ENVIRONMENT
+export POSTGRES_IMAGE=$2:$COMMIT_SHA-$ENVIRONMENT
 
 apt-get install jq -y
 curl -o /usr/local/bin/ecs-cli https://amazon-ecs-cli.s3.amazonaws.com/ecs-cli-linux-amd64-latest
 echo "$(curl -s https://amazon-ecs-cli.s3.amazonaws.com/ecs-cli-linux-amd64-latest.md5) /usr/local/bin/ecs-cli" | md5sum -c -
 chmod +x /usr/local/bin/ecs-cli
 
-ecs-cli configure --cluster $CLUSTER_NAME --default-launch-type EC2 --config-name $CLUSTER_CONFIG_NAME --region ap-south-1
-ecs-cli configure profile --access-key $AWS_ACCESS_KEY --secret-key $AWS_SECRET_KEY --profile-name $CLUSTER_PROFILE_NAME
+ecs-cli configure --cluster "$CLUSTER_NAME" --default-launch-type EC2 --config-name "$CLUSTER_CONFIG_NAME" --region ap-south-1
+ecs-cli configure profile --access-key "$AWS_ACCESS_KEY" --secret-key "$AWS_SECRET_KEY" --profile-name "$CLUSTER_PROFILE_NAME"
 
 ./aws_check_cluster_status.sh
 
 ecs-cli registry-creds up ./creds_input_file.yml --role-name catalystSecretsExecutionRoleCI
 
 echo  "using image.. $BOOKING_IMAGE"
-ecs-cli compose --project-name $ENVIRONMENT service rm --cluster-config $CLUSTER_CONFIG_NAME --ecs-profile $CLUSTER_PROFILE_NAME
-ecs-cli compose --project-name $ENVIRONMENT down --cluster-config $CLUSTER_CONFIG_NAME --ecs-profile $CLUSTER_PROFILE_NAME
-ecs-cli compose --project-name $ENVIRONMENT --cluster-config $CLUSTER_CONFIG_NAME --ecs-profile $CLUSTER_PROFILE_NAME up --create-log-groups
 
-ecs-cli compose --project-name $ENVIRONMENT down --cluster-config $CLUSTER_CONFIG_NAME --ecs-profile $CLUSTER_PROFILE_NAME
-ecs-cli compose --project-name $ENVIRONMENT service up --cluster-config $CLUSTER_CONFIG_NAME --ecs-profile $CLUSTER_PROFILE_NAME
-
-ecs-cli ps --cluster-config $CLUSTER_CONFIG_NAME --ecs-profile $CLUSTER_PROFILE_NAME
-ecs-cli compose service ps --cluster-config $CLUSTER_CONFIG_NAME --ecs-profile $CLUSTER_PROFILE_NAME
+ecs-cli compose --project-name "$ENVIRONMENT" down --cluster-config "$CLUSTER_CONFIG_NAME" --ecs-profile "$CLUSTER_PROFILE_NAME"
+ecs-cli compose --project-name "$ENVIRONMENT" --cluster-config "$CLUSTER_CONFIG_NAME" --ecs-profile "$CLUSTER_PROFILE_NAME" up --create-log-groups
+ecs-cli ps --cluster-config "$CLUSTER_CONFIG_NAME" --ecs-profile "$CLUSTER_PROFILE_NAME"
 
 rm -rf ecs-registry-creds_*.yml
