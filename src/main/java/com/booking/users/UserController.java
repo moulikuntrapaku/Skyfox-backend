f745f1e0bf1a519a2b845f2f969f31729106f24e
Riju Nayak <riju.nayak@gmail.com>
2020-04-29 23:59:14 +0530

    [Riju] Add. Endpoint to login

---
+++ src/main/java/com/booking/users/UserController.java
@@ -0,0 +1,30 @@
+package com.booking.users;
+
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
+
+@Api(tags = "Users")
+@RestController
+public class UserController {
+    UserPrincipalService userPrincipalService;
+
+    @Autowired
+    public UserController(UserPrincipalService userPrincipalService) {
+        this.userPrincipalService = userPrincipalService;
+    }
+
+    @GetMapping("/login")
+    Map<String, Object> login(Principal principal) {
+        String username = principal.getName();
+        Map<String, Object> userDetails = new HashMap<>();
+        userDetails.put("username", username);
+        return userDetails;
+    }
+}
