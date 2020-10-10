5aa1aeea6a152138a4d6d1226dea5026e3506ac7
kaleeaswari <skaleeas@thoughtworks.com>
2020-10-10 12:30:41 +0530

    Adding integration tests for User Login

---
+++ src/test/java/com/booking/users/view/UserControllerIntegrationTest.java
@@ -0,0 +1,56 @@
+package com.booking.users.view;
+
+import com.booking.App;
+import com.booking.users.User;
+import com.booking.users.UserRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(classes = App.class)
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+class UserControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @BeforeEach
+    public void before() {
+        userRepository.deleteAll();
+    }
+
+    @AfterEach
+    public void after() {
+        userRepository.deleteAll();
+    }
+
+
+    @Test
+    public void shouldLoginSuccessfully() throws Exception {
+        userRepository.save(new User("test-user", "password"));
+        mockMvc.perform(get("/login")
+                .with(httpBasic("test-user", "password")))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void shouldThrowErrorMessageForInvalidCredentials() throws Exception {
+        mockMvc.perform(get("/login"))
+                .andExpect(status().isUnauthorized());
+    }
+
+}
