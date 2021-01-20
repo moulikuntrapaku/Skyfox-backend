25809dce6c2a0afed4391a0b46351d1130183b46
Dhruv Kapur <dhruvkapur91@gmail.com>
2021-01-20 15:27:22 +0530

    Fix. Need to remove quotes from everything?

--- scripts/test_deployment.sh
+++ scripts/test_deployment.sh
@@ -14,18 +14,18 @@ export BATCH_ID=${identifiers[1]}
 export TEAM_ID=${identifiers[3]}
 export ENVIRONMENT=${identifiers[6]}
 export PREFIX="/neev-$BATCH_ID/team-$TEAM_ID/$ENVIRONMENT"
-export POSTGRES_IMAGE=`aws ssm get-parameters --name "$PREFIX/POSTGRES_IMAGE" | jq ".Parameters[0].Value"`
-export HOST_DB_PORT=`aws ssm get-parameters --name "$PREFIX/HOST_DB_PORT" | jq ".Parameters[0].Value" | tr -d \"`
-export DB_HOST=`aws ssm get-parameters --name "$PREFIX/DB_HOST" | jq ".Parameters[0].Value"`
+export POSTGRES_IMAGE=`aws ssm get-parameters --name "$PREFIX/POSTGRES_IMAGE" | jq ".Parameters[0].Value" | tr -d \"`
+export HOST_DB_PORT=`aws ssm get-parameters --name "$PREFIX/HOST_DB_PORT" | jq ".Parameters[0].Value"  | tr -d \"`
+export DB_HOST=`aws ssm get-parameters --name "$PREFIX/DB_HOST" | jq ".Parameters[0].Value" | tr -d \"`
 export DB_PORT=`aws ssm get-parameters --name "$PREFIX/DB_PORT" | jq ".Parameters[0].Value" | tr -d \"`
-export POSTGRES_USERNAME=`aws ssm get-parameters --name "$PREFIX/POSTGRES_USERNAME" | jq ".Parameters[0].Value"`
-export DB_NAME=`aws ssm get-parameters --name "$PREFIX/DB_NAME" | jq ".Parameters[0].Value"`
-export UI_HOST=`aws ssm get-parameters --name "$PREFIX/UI_HOST" | jq ".Parameters[0].Value"`
-export MOVIE_SERVICE_HOST=`aws ssm get-parameters --name "$PREFIX/MOVIE_SERVICE_HOST" | jq ".Parameters[0].Value"`
+export POSTGRES_USERNAME=`aws ssm get-parameters --name "$PREFIX/POSTGRES_USERNAME" | jq ".Parameters[0].Value" | tr -d \"`
+export DB_NAME=`aws ssm get-parameters --name "$PREFIX/DB_NAME" | jq ".Parameters[0].Value" | tr -d \"`
+export UI_HOST=`aws ssm get-parameters --name "$PREFIX/UI_HOST" | jq ".Parameters[0].Value" | tr -d \"`
+export MOVIE_SERVICE_HOST=`aws ssm get-parameters --name "$PREFIX/MOVIE_SERVICE_HOST" | jq ".Parameters[0].Value" | tr -d \"`
 export BACKEND_PORT=`aws ssm get-parameters --name "$PREFIX/BACKEND_PORT" | jq ".Parameters[0].Value" | tr -d \"`
-export POSTGRES_PASSWORD=`aws ssm get-parameters --name "$PREFIX/POSTGRES_PASSWORD" | jq ".Parameters[0].Value"`
-export VERSION=`aws ssm get-parameters --name "$PREFIX/VERSION" | jq ".Parameters[0].Value"`
-export BOOKING_IMAGE=`aws ssm get-parameters --name "$PREFIX/BOOKING_IMAGE" | jq ".Parameters[0].Value"`
+export POSTGRES_PASSWORD=`aws ssm get-parameters --name "$PREFIX/POSTGRES_PASSWORD" | jq ".Parameters[0].Value" | tr -d \"`
+export VERSION=`aws ssm get-parameters --name "$PREFIX/VERSION" | jq ".Parameters[0].Value" | tr -d \"`
+export BOOKING_IMAGE=`aws ssm get-parameters --name "$PREFIX/BOOKING_IMAGE" | jq ".Parameters[0].Value" | tr -d \"`
 
 env > /home/ec2-user/envs_available_at_deploytime
 /home/ec2-user/bin/docker-compose down || true
