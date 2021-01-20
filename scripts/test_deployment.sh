3de8881cc45c962201c6d4ec33bff2a02a8c71c4
Dhruv Kapur <dhruvkapur91@gmail.com>
2021-01-20 22:32:00 +0530

    Try. Attempt to create env specific directories myself

--- scripts/test_deployment.sh
+++ scripts/test_deployment.sh
@@ -5,15 +5,25 @@ set -v
 echo "Running this as `whoami`"
 echo "Currently running this in $PWD"
 
-cd /home/ec2-user/deployment/
-echo "Existing contents of the directory are"
-ls
-
 IFS='-' read -ra identifiers <<< $DEPLOYMENT_GROUP_NAME  # DEPLOYMENT_GROUP_NAME is of form neev-xx-team-xx-[backend|frontend]-deployment-[integration|staging|production]
 export BATCH_ID=${identifiers[1]}
 export TEAM_ID=${identifiers[3]}
 export ENVIRONMENT=${identifiers[6]}
 export PREFIX="/neev-$BATCH_ID/team-$TEAM_ID/$ENVIRONMENT"
+
+# horrible hack because of the horrible code deploy programming model!
+# so codedeploy will place artifacts in a directory specified in appspec.yml, but it won't let you parameterize anything in the appspec.yml file
+# this works as long as you have one environment per server, but if you have multiple - it won't work as artifacts will be overriden
+# so one creates this environment specific directories themselves and move the artifacts
+# this probably means that at anytime only one environment could be deployed too (if this works at all)
+# that is, one cannot simultaneously deploy to staging and to integration.
+cd /home/ec2-user/deployment/
+mkdir -p $ENVIRONMENT
+mv docker-compose.yml $ENVIRONMENT/
+cd $ENVIRONMENT
+echo "Existing contents of the directory are"
+ls
+
 export POSTGRES_IMAGE=`aws ssm get-parameters --name "$PREFIX/POSTGRES_IMAGE" | jq ".Parameters[0].Value" | tr -d \"`
 export HOST_DB_PORT=`aws ssm get-parameters --name "$PREFIX/HOST_DB_PORT" | jq ".Parameters[0].Value"  | tr -d \"`
 export DB_HOST=`aws ssm get-parameters --name "$PREFIX/DB_HOST" | jq ".Parameters[0].Value" | tr -d \"`
