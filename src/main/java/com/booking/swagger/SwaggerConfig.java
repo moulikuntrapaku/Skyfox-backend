f7f80f1efe3a43b3730c60f06b0365bd1a72c931
YashasreeTummala <yashasree.tummala@thoughtworks.com>
2022-02-02 17:23:38 +0530

    [Yash] FIX. Upgrade swagger jars to the latest version to solve security vulnerablility

--- src/main/java/com/booking/swagger/SwaggerConfig.java
+++ src/main/java/com/booking/swagger/SwaggerConfig.java
@@ -1,6 +1,5 @@
 package com.booking.swagger;
 
-import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -10,6 +9,7 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import java.util.function.Predicate;
 
 @Configuration
 @EnableSwagger2
@@ -22,7 +22,7 @@ public class SwaggerConfig implements WebMvcConfigurer {
                 .useDefaultResponseMessages(false)
                 .select()
                 .apis(RequestHandlerSelectors.any())
-                .paths(Predicates.not(PathSelectors.regex("/error")))
+                .paths(Predicate.not(PathSelectors.regex("/error")))
                 .build();
     }
 
